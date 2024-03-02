8ae4a7c4e124d0292f48406b3a826172407c986e
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2024-03-03 01:25:54 +0900

    wip

--- chunithm/assets/js/main.js
+++ chunithm/assets/js/main.js
@@ -1,6 +1,7 @@
 import van from "../../../commonassets/js/van-1.3.0.min.js"
+import { musicsData, jacketURLbase } from "./constants.js"
 
-const { button, div, input, section } = van.tags
+const { button, div, img,  input, section } = van.tags
 
 const inputArea = () => div(
     { class: "input-area" },
@@ -8,9 +9,17 @@ const inputArea = () => div(
     button({ id: "answer" }, "回答"),
 )
 
-const songsArea = () => div(
-    { class: "songs-area" },
-)
+const songsArea = () => {
+    const songs = musicsData.map((m) => {
+        return m.meta.genre === "ORIGINAL" ? img(
+            { class: "music-jacket", src: jacketURLbase + md5(m.meta.title + m.meta.artist) + ".webp" },
+        ) : null
+    })
+    return div(
+        { class: "songs-area" },
+        ...songs
+    )
+}
 
 const app = section(
     {id: "main-play-area"},
