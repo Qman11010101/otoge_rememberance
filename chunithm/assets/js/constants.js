d0cd6a38d4775afb4132cf931e25336beda2d19d
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2024-03-03 02:47:56 +0900

    wip

--- chunithm/assets/js/constants.js
+++ chunithm/assets/js/constants.js
@@ -1,4 +1,4 @@
 export const jacketURLbase = "https://reiwa.f5.si/musicjackets/chunithm/"
 
 const musicsURL = "https://reiwa.f5.si/chunirec_all.json"
-export const musicsData = await (await fetch(musicsURL)).json()
+export const musicsDataRaw = await (await fetch(musicsURL)).json()
--- chunithm/assets/js/main.js
+++ chunithm/assets/js/main.js
@@ -1,28 +1,69 @@
 import van from "../../../commonassets/js/van-1.3.0.min.js"
-import { musicsData, jacketURLbase } from "./constants.js"
+import { musicsDataRaw, jacketURLbase } from "./constants.js"
 
-const { button, div, img,  input, section } = van.tags
+const { button, div, img, input, section } = van.tags
+
+const musicsData = musicsDataRaw.map((m) => {
+    return m.meta.genre === "ORIGINAL" || m.meta.genre === "イロドリミドリ" ? {
+        title: m.meta.title,
+        artist: m.meta.artist,
+        jacket: "../../../commonassets/img/unknown.png",
+        lev_mas: m.data.MAS.const,
+        found: false
+    } : null
+}
+).filter((s) => s !== null)
 
 const inputArea = () => div(
     { class: "input-area" },
     input({ id: "song-name", type: "text", placeholder: "入力してください……" }),
-    button({ id: "answer" }, "回答"),
+    button(
+        {
+            id: "answer",
+            onclick: () => {
+                const songName = document.getElementById("song-name").value
+                for (let i = 0; i < musicsData.length; i++) {
+                    if (musicsData[i].title === songName) {
+                        musicsData[i].found = true
+                        const title = musicsData[i].title
+                        const artist = musicsData[i].artist
+                        const jacket = jacketURLbase + md5(title + artist) + ".webp"
+                        musicsData[i].jacket = jacket
+                        const se = document.getElementById("songs-area")
+                        se.children[i].setAttribute("src", jacket)
+                        se.children[i].scrollIntoView({ behavior: "smooth" })
+                        document.getElementById("song-name").value = ""
+                        break
+                    }
+                }
+            },
+        },
+        "回答"
+    ),
 )
 
 const songsArea = () => {
-    const songs = musicsData.map((m) => {
-        return m.meta.genre === "ORIGINAL" ? img(
-            { class: "music-jacket", src: jacketURLbase + md5(m.meta.title + m.meta.artist) + ".webp" },
-        ) : null
-    })
+    let songs = []
+    for (let i = 0; i < musicsData.length; i++) {
+        songs.push(
+            img(
+                {
+                    class: "music-jacket",
+                    "data-title": musicsData[i].title,
+                    "data-artist": musicsData[i].artist,
+                    src: musicsData[i].jacket,
+                }
+            )
+        )
+    }
     return div(
-        { class: "songs-area" },
+        { class: "songs-area", id: "songs-area" },
         ...songs
     )
 }
 
 const app = section(
-    {id: "main-play-area"},
+    { id: "main-play-area" },
     inputArea(),
     songsArea()
 )
